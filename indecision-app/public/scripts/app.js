a2a07ecd127b18c660821b18a386f234d2a634e0
somleff <somleff@gmail.com>
2021-04-07 15:59:47 +0200

    Install Babel

--- indecision-app/public/scripts/app.js
+++ indecision-app/public/scripts/app.js
@@ -1,10 +1,7 @@
-console.log('App is running');
+"use strict";
 
-//JSX - JavaScript XML
-// let template = <p>This is JSX</p>;
-let template = /*#__PURE__*/React.createElement("p", {
-  id: "app"
-}, "This is JSX");
-let appRoot = document.getElementById('app');
+console.log('App is running'); //JSX - JavaScript XML
 
+var template = /*#__PURE__*/React.createElement("h1", null, "Indecision App");
+var appRoot = document.getElementById('app');
 ReactDOM.render(template, appRoot);
---
+++ indecision-app/src/app.js
@@ -0,0 +1,7 @@
+console.log('App is running');
+
+//JSX - JavaScript XML
+let template = <h1>Indecision App</h1>;
+let appRoot = document.getElementById('app');
+
+ReactDOM.render(template, appRoot);
