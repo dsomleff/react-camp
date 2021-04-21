14fce57407c81f54473afacee7db8335195268d3
somleff <somleff@gmail.com>
2021-04-21 13:18:32 +0200

    ES6 Classes p2

--- indecision-app/public/scripts/app.js
+++ indecision-app/public/scripts/app.js
@@ -1,11 +1,34 @@
 "use strict";
 
+function _typeof(obj) { "@babel/helpers - typeof"; if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
+
+function _get(target, property, receiver) { if (typeof Reflect !== "undefined" && Reflect.get) { _get = Reflect.get; } else { _get = function _get(target, property, receiver) { var base = _superPropBase(target, property); if (!base) return; var desc = Object.getOwnPropertyDescriptor(base, property); if (desc.get) { return desc.get.call(receiver); } return desc.value; }; } return _get(target, property, receiver || target); }
+
+function _superPropBase(object, property) { while (!Object.prototype.hasOwnProperty.call(object, property)) { object = _getPrototypeOf(object); if (object === null) break; } return object; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, writable: true, configurable: true } }); if (superClass) _setPrototypeOf(subClass, superClass); }
+
+function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
+
+function _createSuper(Derived) { var hasNativeReflectConstruct = _isNativeReflectConstruct(); return function _createSuperInternal() { var Super = _getPrototypeOf(Derived), result; if (hasNativeReflectConstruct) { var NewTarget = _getPrototypeOf(this).constructor; result = Reflect.construct(Super, arguments, NewTarget); } else { result = Super.apply(this, arguments); } return _possibleConstructorReturn(this, result); }; }
+
+function _possibleConstructorReturn(self, call) { if (call && (_typeof(call) === "object" || typeof call === "function")) { return call; } return _assertThisInitialized(self); }
+
+function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
+
+function _isNativeReflectConstruct() { if (typeof Reflect === "undefined" || !Reflect.construct) return false; if (Reflect.construct.sham) return false; if (typeof Proxy === "function") return true; try { Boolean.prototype.valueOf.call(Reflect.construct(Boolean, [], function () {})); return true; } catch (e) { return false; } }
+
+function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf : function _getPrototypeOf(o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }
+
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
 function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
 
 function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
 
+/**
+ * Parent Class.
+ */
 var Person = /*#__PURE__*/function () {
   function Person() {
     var name = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 'Anonymous';
@@ -31,10 +54,90 @@ var Person = /*#__PURE__*/function () {
 
   return Person;
 }();
+/**
+ * Extended Class (Child).
+ */
 
-var me = new Person('Ju Ju', 18);
+
+var Student = /*#__PURE__*/function (_Person) {
+  _inherits(Student, _Person);
+
+  var _super = _createSuper(Student);
+
+  function Student(name, age, major) {
+    var _this;
+
+    _classCallCheck(this, Student);
+
+    _this = _super.call(this, name, age);
+    _this.major = major;
+    return _this;
+  }
+
+  _createClass(Student, [{
+    key: "hasMajor",
+    value: function hasMajor() {
+      return !!this.major;
+    }
+  }, {
+    key: "getDescription",
+    value: function getDescription() {
+      var description = _get(_getPrototypeOf(Student.prototype), "getDescription", this).call(this);
+
+      if (this.hasMajor()) {
+        description += " Their major is ".concat(this.major, ".");
+      }
+
+      return description;
+    }
+  }]);
+
+  return Student;
+}(Person);
+/**
+ * Home Work  Class
+ */
+
+
+var Traveler = /*#__PURE__*/function (_Person2) {
+  _inherits(Traveler, _Person2);
+
+  var _super2 = _createSuper(Traveler);
+
+  function Traveler(name, age, homeLocation) {
+    var _this2;
+
+    _classCallCheck(this, Traveler);
+
+    _this2 = _super2.call(this, name, age);
+    _this2.homeLocation = homeLocation;
+    return _this2;
+  }
+
+  _createClass(Traveler, [{
+    key: "getGreeting",
+    value: function getGreeting() {
+      var greeting = _get(_getPrototypeOf(Traveler.prototype), "getGreeting", this).call(this);
+
+      if (this.homeLocation) {
+        return greeting += " I'm from ".concat(this.homeLocation);
+      }
+
+      return greeting;
+    }
+  }]);
+
+  return Traveler;
+}(Person);
+/** Class Calls */
+// const me = new Student('Ju Ju', 18, 'Software Engineer');
+
+
+var me = new Traveler('Ju Ju', 18, 'Kurahove'); // console.log(me.hasMajor());
+// console.log(me.getDescription());
+
 console.log(me.getGreeting());
-console.log(me.getDescription());
-var cat = new Person();
+var cat = new Traveler(undefined, undefined, 'Nowhere'); // console.log(cat.hasMajor());
+// console.log(cat.getDescription());
+
 console.log(cat.getGreeting());
-console.log(cat.getDescription());
--- indecision-app/src/playground/es6-classes-1.js
+++ indecision-app/src/playground/es6-classes-1.js
@@ -1,3 +1,6 @@
+/**
+ * Parent Class.
+ */
 class Person {
   constructor(name = 'Anonymous', age = 0) {
     this.name = name;
@@ -13,10 +16,58 @@ class Person {
   }
 }
 
-const me = new Person('Ju Ju', 18);
-console.log(me.getGreeting());
-console.log(me.getDescription());
+/**
+ * Extended Class (Child).
+ */
+class Student extends Person {
+  constructor(name, age, major) {
+    super(name, age);
+    this.major = major;
+  }
 
-const cat = new Person();
+  hasMajor() {
+    return !!this.major;
+  }
+
+  getDescription() {
+    let description = super.getDescription();
+
+    if (this.hasMajor()) {
+      description += ` Their major is ${this.major}.`;
+    }
+
+    return description;
+  }
+}
+
+/**
+ * Home Work  Class
+ */
+class Traveler extends Person {
+  constructor(name, age, homeLocation) {
+    super(name, age);
+    this.homeLocation = homeLocation;
+  }
+
+  getGreeting() {
+    let greeting = super.getGreeting();
+
+    if (this.homeLocation) {
+      return greeting += ` I'm from ${this.homeLocation}`;
+    }
+
+    return greeting;
+  }
+}
+
+/** Class Calls */
+// const me = new Student('Ju Ju', 18, 'Software Engineer');
+const me = new Traveler('Ju Ju', 18, 'Kurahove');
+// console.log(me.hasMajor());
+// console.log(me.getDescription());
+console.log(me.getGreeting());
+
+const cat = new Traveler(undefined, undefined, 'Nowhere');
+// console.log(cat.hasMajor());
+// console.log(cat.getDescription());
 console.log(cat.getGreeting());
-console.log(cat.getDescription());
