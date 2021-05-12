27cf51c6ddb9722b4407eabb165359c6f115db4f
somleff <somleff@gmail.com>
2021-05-12 14:44:33 +0200

    Styling the Options List

--- indecision-app/src/components/IndecisionApp.js
+++ indecision-app/src/components/IndecisionApp.js
@@ -90,20 +90,22 @@ export default class IndecisionApp extends React.Component {
 
 				<div className="container">
 					<Action
-					hasOptions={ this.state.options.length > 0 }
-					handlePick={ this.handlePick }
-				/>
+						hasOptions={ this.state.options.length > 0 }
+						handlePick={ this.handlePick }
+					/>
 
-				<Options
-					options={ this.state.options }
-					handleDeleteOptions={ this.handleDeleteOptions }
-					handleDeleteOption={ this.handleDeleteOption }
-				/>
+					<div className="widget">
+						<Options
+							options={ this.state.options }
+							handleDeleteOptions={ this.handleDeleteOptions }
+							handleDeleteOption={ this.handleDeleteOption }
+						/>
 
-				<AddOption
-					handleAddOption={ this.handleAddOption }
-				/>
-			</div>
+						<AddOption
+							handleAddOption={ this.handleAddOption }
+						/>
+					</div>
+				</div>
 
 				<OptionModal
 					selectedOption={ this.state.selectedOption }
