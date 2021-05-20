1395a4d398f3fd2867a36bb5af5a39c17aa35674
somleff <somleff@gmail.com>
2021-05-20 10:53:55 +0200

    Boilerplate and New Project

---
+++ expensify-app/src/app.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'normalize.css/normalize.css';
+import './styles/styles.scss';
+
+/** Usage Components Section */
+ReactDOM.render(<p>This is Boilerplate!</p>, document.getElementById('app'));
