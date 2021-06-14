6f7da97af72bfd159334fdc1c242fef247a8bab7
dsomleff <somleff@gmail.com>
2021-06-14 10:51:20 +0200

    Edit Expense and set Text Filter

--- expensify-app/src/playground/redux-expensify.js
+++ expensify-app/src/playground/redux-expensify.js
@@ -19,9 +19,9 @@ const demoState = {
 };
 
 /*
- * Actions
+ * Actions for Expense Reduser
 */
-// ADD EXPENSE
+// ADD EXPENSE ACTION
 const addExpense = (
   {
     description = '',
@@ -40,10 +40,17 @@ const addExpense = (
   }
 });
 
-// REMOVE EXPENSE
+// REMOVE EXPENSE ACTION
 const removeExpense = ({ id } = {}) => ({
   type: 'REMOVE_EXPENSE',
   id
+});
+
+// EDIT EXPENSE ACTION
+const editExpense = (id, updates) => ({
+  type: 'EDIT_EXPENSE',
+  id,
+  updates
 });
 
 /*
@@ -57,10 +64,30 @@ const expensesReducer = (state = expensesReducerDefaultState, action) => {
       return [...state, action.expense];
     case 'REMOVE_EXPENSE':
       return state.filter((expenses) => expenses.id !== action.id);
+    case 'EDIT_EXPENSE':
+      return state.map((expense) => {
+        if (expense.id === action.id) {
+          return {
+            ...expense,
+            ...action.updates
+          }
+        } else {
+          return expense;
+        }
+      });
     default:
       return state;
   }
 };
+
+/*
+ * Actions for Filter Reduser
+*/
+// SET TEXT FILTER ACTION
+const setTextFilter = (text = '') => ({
+  type: 'SET_TEXT_FILTER',
+  text
+});
 
 /*
  * Filter Reducer
@@ -74,6 +101,11 @@ const filtersReducerDefaultState = {
 
 const filtersReducer = (state = filtersReducerDefaultState, action) => {
   switch(action.type) {
+    case 'SET_TEXT_FILTER':
+      return {
+        ...state,
+        text: action.text
+      };
     default:
       return state;
   }
@@ -97,3 +129,7 @@ const expenseOne = store.dispatch(addExpense({ description: 'Rent', amount: 100 
 const expenseTwo = store.dispatch(addExpense({ description: 'Coffee', amount: 300 }));
 
 store.dispatch(removeExpense({ id: expenseOne.expense.id }));
+store.dispatch(editExpense(expenseTwo.expense.id, { amount: 500 }));
+
+store.dispatch(setTextFilter('rent'));
+store.dispatch(setTextFilter(''));
