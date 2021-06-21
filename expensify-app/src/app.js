12ac334f99575829a687f67ffb8e3357b633000a
dsomleff <somleff@gmail.com>
2021-06-21 14:52:38 +0200

    Rendering Individual Expenses

--- expensify-app/src/app.js
+++ expensify-app/src/app.js
@@ -12,7 +12,7 @@ import './styles/styles.scss';
 /** Usage Redux Store */
 const store = configureStore();
 
-store.dispatch(addExpense({ description: 'Water bill' }));
+store.dispatch(addExpense({ description: 'Water bill', amount: 4500 }));
 store.dispatch(addExpense({ description: 'Gas bill' }));
 store.dispatch(setTextFilter('water'));
 
