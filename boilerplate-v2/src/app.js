09a6106bc329daebf3461d001473a465bbd502f4
Dmytro Somliev <77507444+dsomleff@users.noreply.github.com>
2021-09-08 10:47:23 +0200

    Boulerplate with Router

---
+++ boilerplate-v2/src/app.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import AppRouter from './routers/AppRouter';
+import configureStore from './store/configureStore';
+import 'normalize.css/normalize.css';
+import './styles/styles.scss';
+import 'react-dates/lib/css/_datepicker.css';
+import 'react-dates/initialize';
+import './firebase/firebase';
+import LoadingPage from './components/LoadingPage';
+
+/** Usage Redux Store */
+const store = configureStore();
+
+/** Usage Components Section */
+const jsx = (
+  <Provider store={ store }>
+    <AppRouter />
+  </Provider>
+);
+ReactDOM.render(<LoadingPage />, document.getElementById('app'));
+ReactDOM.render(jsx, document.getElementById('app'));
---
+++ boilerplate-v2/src/store/configureStore.js
@@ -0,0 +1,18 @@
+/*
+ * Store creation
+*/
+import { createStore, combineReducers, applyMiddleware,compose } from 'redux';
+import thunk from 'redux-thunk';
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSIONS_COMPOSE__ || compose;
+
+export default () => {
+  const store = createStore(
+    combineReducers({
+
+    }),
+    composeEnhancers(applyMiddleware(thunk))
+  );
+
+  return store;
+};
