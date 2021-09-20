0024d9fbd1975e50c37ea64cf8162357215394f0
Dmytro Somliev <77507444+dsomleff@users.noreply.github.com>
2021-09-20 14:29:29 +0200

    Breaking Home Page into Components

---
+++ clothing-app/src/pages/homepage/HomePage.jsx
@@ -0,0 +1,10 @@
+import Directory from '../../components/directory/Directory';
+import './homepage.styles.scss';
+
+const HomePage = () => (
+  <div className="homepage">
+    <Directory />
+  </div>
+);
+
+export default HomePage;
