99e2d58381acad624bab15c75006b6afdb686f3e
Dmytro Somliev <77507444+dsomleff@users.noreply.github.com>
2021-09-30 15:59:56 +0200

    Cart Button FBC

--- hooksmeal-app/src/components/Layout/Header.js
+++ hooksmeal-app/src/components/Layout/Header.js
@@ -1,13 +1,14 @@
 import { Fragment } from 'react';
 import styles from './Header.module.css';
 import mealsImage from '../../assets/meals2.jpg';
+import HeaderCartButton from './HeaderCartButton';
 
 const Header = () => {
   return (
     <Fragment>
       <header className={styles.header}>
         <h1>HooksMeal</h1>
-        <button>Cart</button>
+        <HeaderCartButton />
       </header>
 
       <div className={styles['main-image']}>
