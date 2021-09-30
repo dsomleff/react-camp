aa53e5fdfdfcb46b422be743fd407385ceb59db7
Dmytro Somliev <77507444+dsomleff@users.noreply.github.com>
2021-09-30 16:40:48 +0200

    MealItem FBCs

--- hooksmeal-app/src/components/Meals/AvailableMeals.js
+++ hooksmeal-app/src/components/Meals/AvailableMeals.js
@@ -1,14 +1,25 @@
 import styles from './AvailableMeals.module.css';
 import meals from './dummy-meals';
+import Card from '../UI/Card';
+import MealItem from './MealItem/MealItem';
 
 const AvailableMeals = () => {
-  const mealsList = meals.map(meal => <li>{meal.name}</li>);
+  const mealsList = meals.map(meal => (
+    <MealItem
+      key={meal.id}
+      name={meal.name}
+      description={meal.description}
+      price={meal.price}
+    />
+  ));
 
   return (
     <section className={styles.meals}>
-      <ul>
-        {mealsList}
-      </ul>
+      <Card>
+        <ul>
+          {mealsList}
+        </ul>
+      </Card>
     </section>
   );
 };
