f02afc7cf328d2ad9ea6f4598c7854a0d30cfa61
Dmytro Somliev <77507444+dsomleff@users.noreply.github.com>
2021-10-04 14:44:40 +0200

    Brain cracker CartReducer logic

--- hooksmeal-app/src/reducers/CartReducer.js
+++ hooksmeal-app/src/reducers/CartReducer.js
@@ -1,9 +1,24 @@
 const CartReducer = (cart, action) => {
   switch (action.type) {
     case 'ADD_ITEM':
-      const items = cart.items.concat(action.item);
       const totalAmount = cart.totalAmount + action.item.price * action.item.amount;
-      return { items, totalAmount };
+      // check if item already added in to a cart
+      const addedCartItemIndex = cart.items.findIndex(item => item.id === action.item.id);
+      const addedCartItem = cart.items[addedCartItemIndex]; // grab existed item
+      let addedItems; // all items in cart
+
+      if (addedCartItem) {
+        const addedItem = {
+          ...addedCartItem,
+          amount: addedCartItem.amount + action.item.amount
+        };
+        addedItems = [...cart.items];
+        addedItems[addedCartItemIndex] = addedItem;
+      } else {
+        addedItems = cart.items.concat(action.item);
+      }
+
+      return { items: addedItems, totalAmount };
     default:
       return cart;
   }
