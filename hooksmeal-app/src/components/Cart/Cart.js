11b4b30a96fe5c361547aaac3081d0d50850b3a7
Dmytro Somliev <77507444+dsomleff@users.noreply.github.com>
2021-10-04 12:10:09 +0200

    Outputting Cart Items

--- hooksmeal-app/src/components/Cart/Cart.js
+++ hooksmeal-app/src/components/Cart/Cart.js
@@ -1,11 +1,34 @@
 import Modal from '../UI/Modal';
 import styles from './Cart.module.css';
+import { useContext } from 'react';
+import CartContext from '../../context/cart-context';
+import CartItem from './CartItem';
 
 const Cart = props => {
-  const cartItems = <ul className={styles['cart-items']}>{[
-    { id: 1, name: 'Sushi', amount: 2, price: 12.99 }]
-    .map(item => <li>{item.name}</li>
-    )}</ul>;
+  const cartContext = useContext(CartContext);
+  const totalAmount = `$${cartContext.totalAmount.toFixed(2)}`;
+  const hasItems = cartContext.items.length > 0;
+
+  const cartItemRemove = id => {
+
+  };
+
+  const cartItemAdd = item => {
+
+  };
+
+  const cartItems = (
+    <ul className={styles['cart-items']}>
+      {cartContext.items.map(item =>
+        <CartItem
+          key={item.id}
+          {...item}
+          onRemove={cartItemRemove.bind(null, item.id)}
+          onAdd={cartItemAdd.bind(null, item)}
+        />
+      )}
+    </ul>
+  );
 
   return (
     <Modal onClick={props.onHideCart}>
@@ -13,7 +36,7 @@ const Cart = props => {
 
       <div className={styles.total}>
         <span>Total Amount</span>
-        <span>36.34 </span>
+        <span>{totalAmount}</span>
       </div>
 
       <div className={styles.actions}>
@@ -23,7 +46,7 @@ const Cart = props => {
         >
           Close
         </button>
-        <button className={styles.button}>Order</button>
+        {hasItems && <button className={styles.button}>Order</button>}
       </div>
     </Modal>
   );
