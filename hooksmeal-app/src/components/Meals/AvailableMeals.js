81305a201950651ae28be5353b672c1f3002552a
Dmytro Somliev <77507444+dsomleff@users.noreply.github.com>
2021-11-04 16:06:56 +0100

    Fetch Error Handling

--- hooksmeal-app/src/components/Meals/AvailableMeals.js
+++ hooksmeal-app/src/components/Meals/AvailableMeals.js
@@ -7,10 +7,16 @@ import MealItem from './MealItem/MealItem';
 const AvailableMeals = () => {
   const [meals, setMeals] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [httpError, setHttpError] = useState();
 
   useEffect(() => {
     const fetchMeals = async () => {
       const response = await fetch('https://hooks-meal-app-default-rtdb.firebaseio.com/meals.json');
+
+      if (!response.ok) {
+        throw new Error('Something went wrong!');
+      }
+
       const data = await response.json();
       const loadedMeals = [];
 
@@ -27,7 +33,10 @@ const AvailableMeals = () => {
       setIsLoading(false);
     };
 
-    fetchMeals();
+    fetchMeals().catch(error => {
+      setIsLoading(false);
+      setHttpError(error.message);
+    });
   }, []);
 
   if (isLoading) {
@@ -35,7 +44,15 @@ const AvailableMeals = () => {
       <section className={styles.MealsLoading}>
         <p>...Loading</p>
       </section>
-    )
+    );
+  }
+
+  if (httpError) {
+    return (
+      <section className={styles.MealsError}>
+        <p>{httpError}</p>
+      </section>
+    );
   }
 
   const mealsList = meals.map(meal => (
