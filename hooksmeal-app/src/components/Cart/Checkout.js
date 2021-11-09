797ca200af39f1ab03489289ab617e957eb7a760
Dmytro Somliev <77507444+dsomleff@users.noreply.github.com>
2021-11-09 15:21:32 +0100

    Add styles and useRef to Order Form Inputs

--- hooksmeal-app/src/components/Cart/Checkout.js
+++ hooksmeal-app/src/components/Cart/Checkout.js
@@ -1,36 +1,67 @@
-import styles from './Cart.module.css';
+import { useRef } from 'react';
+import styles from './Checkout.module.css';
 
-const Checkout = props => {
+const Checkout = (props) => {
+  const nameRef = useRef();
+  const streetRef = useRef();
+  const postalRef = useRef();
+  const cityRef = useRef();
+
   const submitHandler = (event) => {
     event.preventDefault();
-  }
+
+    const nameValue = nameRef.current.value;
+    const streetValue = streetRef.current.value;
+    const postalValue = postalRef.current.value;
+    const cityValue = cityRef.current.value;
+  };
 
   return (
-    <form onSubmit={submitHandler}>
+    <form className={styles.form} onSubmit={submitHandler}>
       <div className={styles.control}>
-        <label htmlFor="name">Your Name</label>
-        <input type="text" id="name" />
+        <label htmlFor='name'>Your Name</label>
+        <input
+          type='text'
+          id='name'
+          ref={nameRef}
+        />
       </div>
 
       <div className={styles.control}>
-        <label htmlFor="street">Street</label>
-        <input type="text" id="street" />
+        <label htmlFor='street'>Street</label>
+        <input
+          type='text'
+          id='street'
+          ref={streetRef}
+        />
       </div>
 
       <div className={styles.control}>
-        <label htmlFor="postal">Postal Code</label>
-        <input type="text" id="postal" />
+        <label htmlFor='postal'>Postal Code</label>
+        <input
+          type='text'
+          id='postal'
+          ref={postalRef}
+        />
       </div>
 
       <div className={styles.control}>
-        <label htmlFor="city ">City</label>
-        <input type="text" id="city" />
+        <label htmlFor='city'>City</label>
+        <input
+          type='text'
+          id='city'
+          ref={cityRef}
+        />
       </div>
 
-      <button type="button">Confirm</button>
-      <button onClick={props.onCancel}>Cancel</button>
+      <div className={styles.actions}>
+        <button type='button' onClick={props.onCancel}>
+          Cancel
+        </button>
+        <button className={styles.submit}>Confirm</button>
+      </div>
     </form>
   );
-}
+};
 
 export default Checkout;
