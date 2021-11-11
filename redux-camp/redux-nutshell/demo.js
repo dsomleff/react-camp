1bd2a807b646bb1dbccd5da65a19f0c5e4a925a0
Dmytro Somliev <77507444+dsomleff@users.noreply.github.com>
2021-11-11 14:24:25 +0100

    Add Actions

--- redux-camp/redux-nutshell/demo.js
+++ redux-camp/redux-nutshell/demo.js
@@ -7,9 +7,19 @@ const redux = require('redux');
  * @returns {new state, which replace existed one} object
  */
 const counterReducer = (state = { counter: 0 }, action) => {
-  return {
-    counter: state.counter + 1
-  };
+  if (action.type === 'INCREMENT') {
+    return {
+      counter: state.counter + 1
+    };
+  }
+
+  if (action.type === 'DECREMENT') {
+    return {
+      counter: state.counter - 1
+    };
+  }
+
+  return state;
 };
 
 /**
@@ -21,6 +31,8 @@ const store = redux.createStore(counterReducer);
 /**
  * Subscription to redux store
  * Will be triggered as soon state was changed.
+ * And then return this state.
+ * Component will apply this role.
  */
 const counterSubscriber = () => {
   const latestState = store.getState(); // receive the latest state snapshot
@@ -33,7 +45,8 @@ const counterSubscriber = () => {
 store.subscribe(counterSubscriber);
 
 /**
- * Dispatch the action
+ * Dispatch the actions
  * Action is an object with type property.
  */
 store.dispatch({ type: 'INCREMENT' });
+store.dispatch({ type: 'DECREMENT' });
